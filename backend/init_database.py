70d8ca96bd00e8609db0f2012c6cece7ec4d0aeb
Niklas271 <nika01nl@gmail.com>
2026-05-31 13:24:29 +0200

    changed init_database to manually add rows

--- backend/init_database.py
+++ backend/init_database.py
@@ -1,8 +1,11 @@
 import zipfile
-import csv_to_sqlite
+import csv
 import sqlite3
 
 DB_FILE = "database.sqlite"
+
+with zipfile.ZipFile("data/DDIDataset.zip", 'r') as zip_ref:
+    zip_ref.extractall("data/")
 
 conn = sqlite3.connect(DB_FILE)
 cursor = conn.cursor()
@@ -34,11 +37,13 @@ cursor.execute("""
         interaction TEXT
     )
 """)
+
+with open('data/DDIDataset.csv','r', encoding='utf-8-sig') as fin:
+    dr = csv.DictReader(fin, delimiter=';')
+    print("Detected headers:", dr.fieldnames)
+    to_db = [(i['Drug1'], i['Drug2'], i['Percentage'], i['Interaction']) for i in dr]
+
+cursor.executemany("INSERT INTO drugs (drug1, drug2, percentage, interaction) VALUES (?, ?, ?, ?);", to_db)
+conn.commit()
+
 conn.close()
-
-with zipfile.ZipFile("data/DDIDataset.zip", 'r') as zip_ref:
-    zip_ref.extractall("data/")
-
-options = csv_to_sqlite.CsvOptions(typing_style="full", encoding="windows-1250", delimiter=";")
-input_files = ["data/DDIDataset.csv"]
-csv_to_sqlite.write_csv(input_files, DB_FILE, options)
